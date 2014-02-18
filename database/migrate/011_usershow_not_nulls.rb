f38a80322de6cf7873348570ae22f0f69b9c6f7d
Georgi Gardev <georgi@gardev.com>
2014-02-18 13:05:44 +0200

    Fix migrations

--- database/migrate/011_usershow_not_nulls.rb
+++ database/migrate/011_usershow_not_nulls.rb
@@ -1,18 +1,13 @@
 Sequel.migration do
   up do
     alter_table :usershows do
-      drop_column :episode
-      add_column :episode, Integer, default: 0, null: false
-
-      drop_column :season
-      add_column :season, Integer, default: 0, null: false
+      set_column_default :episode, 0
+      set_column_default :season, 0
     end
   end
 
   down do
     alter_table :usershows do
-      set_column_allow_null :episode
-      set_column_allow_null :season
       set_column_default :episode, nil
       set_column_default :season, nil
     end
--- database/migrate/012_show_rating_defaults.rb
+++ database/migrate/012_show_rating_defaults.rb
@@ -1,18 +1,13 @@
 Sequel.migration do
   up do
     alter_table :shows do
-      drop_column :rating
-      add_column :rating, Float, default: 0, null: false
-
-      drop_column :rating_count
-      add_column :rating_count, Integer, default: 0, null: false
+      set_column_default :rating, 0
+      set_column_default :rating_count, 0
     end
   end
 
   down do
     alter_table :shows do
-      set_column_allow_null :rating
-      set_column_allow_null :rating_count
       set_column_default :rating, nil
       set_column_default :rating_count, nil
     end
