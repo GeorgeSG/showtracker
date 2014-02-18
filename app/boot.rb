14f3e2fd0e34de87a72ea8d39aacecc2bd724dff
Georgi Gardev <georgi@gardev.com>
2014-02-18 12:16:35 +0200

    Style fixes

--- app/boot.rb
+++ app/boot.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
-require 'bundler/setup'
 require 'yaml'
+require 'bundler/setup'
 Bundler.require :default
 
 # =============================================================================
@@ -11,14 +11,14 @@ config_file  = File.open(File.expand_path('../config/config.yml', __FILE__))
 settings     = YAML.load(config_file)
 app_path     = settings['app_path'] || './'
 
-require_file = lambda { |file| require file }
+require_file = -> (file) { require file }
 
 # These need to be required before the base class, because
-# the base class must register all extensions
-Dir[app_path + '/extensions/**/*.rb'].each &require_file
-Dir[app_path + '/base.rb'].each &require_file
+# the base class must register all extensions.
+Dir[app_path + '/extensions/**/*.rb'].each(&require_file)
+Dir[app_path + '/base.rb'].each(&require_file)
 
 # These need to be required after the base class, because
 # the database connection needs to be set up first, and
-# also the base class must be defined to subclass it
-Dir[app_path + '/{models,helpers,controllers}/**/*.rb'].each &require_file
+# also the base class must be defined to subclass it.
+Dir[app_path + '/{models,helpers,controllers}/**/*.rb'].each(&require_file)
